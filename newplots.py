caa38e386e7e56cb37e5835d02ef72ea22278b6f
Labibahx <aycha.tammour@gmail.com>
2015-05-28 19:17:20 -0400

    plotting line profiles and other scatter plots

--- newplots.py
+++ newplots.py
@@ -94,7 +94,7 @@ def profiles(line, k1, k2):
 
     fl= range(1,5)
 
-    clr_ls= ['orange', 'navy', 'mediumvioletred','seagreen', 'brown' ,'cornflowerblue', 'khaki', 'olive', 'purple']
+    clr_ls= ['orange', 'navy', 'mediumvioletred','seagreen', 'khaki', 'cornflowerblue', 'brown' , 'olive', 'purple']
     xlimit= [(1500,1600), (1600, 1700), (1835, 1950), (2740, 2850)]
     ylimit= [(0.55,2.1), (0.49,0.9), (0.39,0.9), (0.19,0.5)]
     line_mark= [[1549], [1640, 1663.5], [1857, 1892, 1908], [2800]]
@@ -113,7 +113,7 @@ def profiles(line, k1, k2):
         for ll in range(len(l_lab)):
             
             ax.axvline(l_mark[ll], ls= ':', c= 'k')
-            ax.text(l_mark[ll]+2, yl[1]-yl[1]/15, l_lab[ll])
+            ax.text(l_mark[ll]-4, yl[1]-yl[1]/20, l_lab[ll], rotation= 'vertical', horizontalalignment= 'center')
 
 
         ii=1.85
@@ -131,7 +131,7 @@ def profiles(line, k1, k2):
     fl= range(5,9)
 
 
-    clr_ls= ['orange', 'navy', 'mediumvioletred','seagreen', 'brown' ,'cornflowerblue', 'khaki', 'olive', 'purple']
+    clr_ls= ['orange', 'navy', 'mediumvioletred','seagreen', 'khaki', 'cornflowerblue', 'brown' , 'olive', 'purple']
     xlimit= [(1500,1600), (1600, 1700), (1835, 1950), (2740, 2850)]
     ylimit= [(0.55,2.1), (0.49,0.9), (0.39,0.9), (0.19,0.5)]
     line_mark= [[1549], [1640, 1663.5], [1857, 1892, 1908], [2800]]
@@ -150,7 +150,7 @@ def profiles(line, k1, k2):
         for ll in range(len(l_lab)):
             
             ax.axvline(l_mark[ll], ls= ':', c= 'k')
-            ax.text(l_mark[ll]+2, yl[1]-yl[1]/15, l_lab[ll])
+            ax.text(l_mark[ll]-4, yl[1]-yl[1]/20, l_lab[ll], rotation= 'vertical', horizontalalignment= 'center')
 
 
         ii= 1.85
@@ -165,28 +165,61 @@ def profiles(line, k1, k2):
                     ax.text(1510, ii, str(n), color= clr)
 
 
+#####################
 
-""" scatter plots (and histograms??) for each cluster.
-"""
+
+""" 2D scatter plots for clusters. read files from saved 2d numpy arrays
+    the aray has the features in the "columns". the last column has the labels indicating which cluster each sample (row) belongs to.
+    line: the emission line used in clustering: MgII, CIII], or CIV
+    cluster: line_ew_hwhm -in this case. can refere to any other cluster name (eg, mg2_ew_hwhm)
+    k: number of clusters used in creating the loaded numpy array. The labels column in the array goes from 0 to k-1
+    feature 1: one of the features used in the clustering analysis. This will be plotted on the x-axis
+    feature 2: to be plotted on the y-axis
     
+    in the ew_hwhm clusters, the features are EW, BHWHM, and RHWHM given in the 0th, 1st and 2nd vectors on the array.
     
-    clstrs= glob.glob(line+"_ew_hwhm*clstrs.npy")
-    clstrs1= np.load(clstrs[0])
-    clstrs2= np.load(clstrs[1])
-    fig= figure()
-    ax1= fig.add_sublot(241)
-    for c in range(1,k1+1):
-        ax1.plot()
+    """
+
+def two_d_scatter(line, cluster, k, feature1, feature2, feature3):
+
+
+    clstr_name= cluster+"_"+str(k)+"clstrs.npy"
+    clstr= np.load(clstr_name)
     
-    como_list.append(line)
-    orderd_compos= sorted()
-    for f in compos:
-        spec_file= fits.open(f)
+    ew= clstr[:,0] #EW
+    bhwhm= clstr[:,1] #BHWHM
+    rhwhm= clstr[:,2] #RHWHM
+    c_label= clstr[:,3] #label
+    
+    clstr_length=[]
 
-sorted(list, key=itemgetter(1))
+    for c in range(0,k):
+        clstr_length.append([c,len(clstr[c_label==c])])
 
+    ordered_clstrs =sorted(clstr_length, key=itemgetter(1), reverse= True)
+    print ordered_clstrs
+    
+    clr_ls= ['orange', 'navy', 'mediumvioletred','seagreen', 'khaki', 'cornflowerblue', 'brown' , 'olive', 'purple']
 
+    fig= figure(figsize(8,8))
+    ax= fig.add_subplot(111)
 
+    text(0.05, 0.1, 'K= '+ str(k)+ ', Clustering features:'+' '+line+' ('+ feature1+','+feature2+','+ feature3+')',
+         horizontalalignment='left', verticalalignment='center',
+         transform=ax.transAxes, color= 'black')
+
+    xlabel(line+" "+feature1+ " (km/s)")
+    ylabel(line+" "+feature2+ " (km/s)")
+
+    t=0
+    for o in ordered_clstrs:
+        print o[0], o[1]
+        
+        ax.scatter(bhwhm[c_label==o[0]], rhwhm[c_label==o[0]], c=clr_ls[t], label=str(o[1]))
+
+        t+=1
+
+    legend(scatterpoints=1)
 
 
 
