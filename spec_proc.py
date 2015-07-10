5a38f2593068e1e32ec50c93cf04fb3c0ae2a93f
Aycha Tammour <aycha.tammour@gmail.com>
2015-07-10 13:30:12 -0400

    changed binning to delta_lambda = 0.5. Only running on selected sample with my flags. Writing new processed spectra to dir new_proc_spec

--- spec_proc.py
+++ spec_proc.py
@@ -10,7 +10,9 @@ from specutils import extinction
 
 data= Table.read('dr10q.fits')
 
-s = data[(data['Z_PCA'] >1.6) & (data['Z_PCA'] <2.1) & (data['REWE_CIII'] >0) & (data['REWE_CIII'] <2000) & (data['REWE_CIV'] >0) & (data['REWE_CIV'] <2000) & (data['REWE_MGII'] >0) & (data['REWE_MGII'] <2000) & (data['BAL_FLAG_VI'] ==0)] #  subsample with: upper and lower redshift limits, reasonable measurements for EW, and BAL quasars excluded
+s= Table.read('sample_myflags.csv')
+
+#s = data[(data['Z_PCA'] >1.6) & (data['Z_PCA'] <2.1) & (data['REWE_CIII'] >0) & (data['REWE_CIII'] <2000) & (data['REWE_CIV'] >0) & (data['REWE_CIV'] <2000) & (data['REWE_MGII'] >0) & (data['REWE_MGII'] <2000) & (data['BAL_FLAG_VI'] ==0)] #  subsample with: upper and lower redshift limits, reasonable measurements for EW, and BAL quasars excluded
 
 for i in range(len(s)):
     print i
@@ -24,10 +26,10 @@ for i in range(len(s)):
     tau_lambda= ext_lambda/((1 * u.angstrom) * 1.086) # for some reason the extinction has a unit of angstrom (should be magnitude i.e. unitless)
     dered_flx= flx * np.exp(tau_lambda) # extinction corrected flux
     rest_wlen= wlen /(1+z) # restframe wavelength
-    x= np.arange(1100, 4000, 0.1) # the wavelength values for the rebinned spectrum with bin width = 0.1 A
-    rebin_dered_flx= np.interp(x, rest_wlen, dered_flx) # resampled flux with delta lambda= 0.1 A
-    norm_flx= rebin_dered_flx/np.mean(rebin_dered_flx[2360:2390]) # normalize spectra
-    proc_spec= np.vstack((x, norm_flx)) # 2D array. 1st row: restframe wavelength, 2nd row: corrected flux
+    x= np.arange(1100, 4000, 0.5) # the wavelength values for the rebinned spectrum with bin width = 0.5 A
+    rebin_dered_flx= np.interp(x, rest_wlen, dered_flx) # resampled flux with delta lambda= 0.5 A
+   # norm_flx= rebin_dered_flx/np.mean(rebin_dered_flx[2360:2390]) # normalize spectra
+    proc_spec= np.vstack((x, rebin_dered_flx)) # 2D array. 1st row: restframe wavelength, 2nd row: corrected flux
     hdu= fits.PrimaryHDU(proc_spec) # HDU with flux and wavelngth to be saved as a fits file
-    new_spec_name= './proc_data/spec-'+str(s['PLATE'][i])+'-'+str(s['MJD'][i])+'-'+str(s['FIBERID'][i]).zfill(4)+'_proc.fits' # name of new processed spectrum
+    new_spec_name= './new_proc_data/spec-'+str(s['PLATE'][i])+'-'+str(s['MJD'][i])+'-'+str(s['FIBERID'][i]).zfill(4)+'_proc.fits' # name of new processed spectrum
     hdu.writeto(new_spec_name)
