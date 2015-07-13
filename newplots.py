5052ea9f2e0c1171d7e80125dc01b89e9f6a2789
Labibahx <aycha.tammour@gmail.com>
2015-07-13 00:00:58 -0400

    new line profile function (line_profile) is done. used to generate new plots

--- newplots.py
+++ newplots.py
@@ -8,6 +8,7 @@ from sklearn.cluster import KMeans
 from mpl_toolkits.mplot3d import Axes3D
 from operator import itemgetter
 from glob import glob
+import seaborn as sns
 
 
 ss= np.load(dr10qsample.npy)
@@ -24,7 +25,7 @@ def line_profile(line, k):
     """ plot profiles for lines in the same cluster (line+k, e.g, CIV, k=4) in 6 panels for: Ly alpha, Si IV, C IV, He II, (Al III, Si III], C III], Mg II
         """
 
-    comp_name= line+"_ew_hwhm_"+str(k)+"*.fits"
+    compo_name= line+"_ew_hwhm_"+str(k)+"*.fits"
     compos= glob(compo_name)
 
 
@@ -35,12 +36,47 @@ def line_profile(line, k):
         compo_list.append([obj, num_obj])
     ordered_compos= sorted(compo_list, key= itemgetter(1), reverse= True)
 
-    fig= figure()
+    print ordered_compos
 
-    for (p,deltax) in zip(range(7), deltax_list):
+
+    fig= figure(figsize=(12,8))
+    sns.set_style("ticks")
+    fig1= fig.add_axes([0., 0., 1, 1])
+    fig1.set_axis_off()
+    fig1.set_xlim(0, 1)
+    fig1.set_ylim(0, 1)
+    fig1.text(.07, 0.5, r"Normalized Flux (erg s$^{-1}$ cm$^{-1}$ $\AA^{-1}$)", rotation='vertical', horizontalalignment='center', verticalalignment='center')
+    fig1.text(0.5, 0.03, r"Wavelength ($\AA$)", rotation='horizontal', horizontalalignment='center', verticalalignment='center')
+
+    dx_list= [(1160, 1265), (1350, 1450), (1500, 1600), (1590, 1690), (1810, 1950), (2750, 2850)]
+    dy_list= [(0.75, 1.9), (0.75, 1.5), (0.75, 2.53), (0.75, 1.2), (0.75, 1.8), (0.75, 1.6)]
+
+    line_mark= [[1215.7, 1240], [1396.8], [1549], [1640, 1663.5], [1857, 1892, 1908], [2800]]
+    line_label= [[r'Ly$\alpha$', 'N V'], ['Si IV'], ['C IV'], ['He II', 'O III]'], ['Al III', 'Si III]', 'C III]'], ['Mg II']]
+
+    clr_ls= ['orange', 'navy', 'mediumvioletred','seagreen', 'khaki', 'cornflowerblue', 'brown' , 'olive', 'purple']
+
+    wlen= np.arange(1100, 4000, 0.5) #wavelength array
+
+    for (p,dx, dy, lm, lb) in zip(range(1,8), dx_list, dy_list, line_mark, line_label):
         ax= fig.add_subplot(2,3,p)
+        xlim(dx)
+        ylim(dy)
 
+        for ll in range(len(line_mark[p-1])):
+        
+            axvline(lm[ll], ls=':', c='k')
+            text(lm[ll]+0.1, dy[1]-0.1, lb[ll], rotation= 'vertical', fontsize= 'small')
 
+        ii= 2.5
+        for (sp, clr) in zip(ordered_compos, clr_ls):
+            n= sp[1]
+            if sp[1] >100:
+                spec= fits.open(sp[0])
+                plot(wlen, spec[0].data/spec[0].data[(dx[0]-1100)*2], c= clr, lw= 2)
+                ii-=0.15
+                if p==3:
+                    ax.text(1510, ii, str(n), color= clr)
 
 
 
@@ -80,7 +116,7 @@ def profiles(line, k1, k2):
     fig1.text(0.92, 0.7, "K = "+str(k1), rotation= 'vertical', horizontalalignment= 'center')
     fig1.text(0.92, 0.3, "K = "+str(k2), rotation= 'vertical', horizontalalignment= 'center')
     
-    w= np.arange(1100, 4000, 0.1) #wavelength array
+    w= np.arange(1100, 4000, 0.5) #wavelength array
 
     fl= range(1,5)
 
