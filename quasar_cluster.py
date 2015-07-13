0d49fa2658e7cd7b36b058dc3ad65d249149b2df
Labibahx <aycha.tammour@gmail.com>
2015-07-13 10:01:10 -0400

    worked on silhouette scores

--- quasar_cluster.py
+++ quasar_cluster.py
@@ -91,7 +91,7 @@ features= [[c4_ew, c4_bhwhm, c4_rhwhm],
 
 # qs= np.column_stack(n for n in features[0][:3]+features[1][:3]+features[2][:3]) # all three lines. can specify which features to use by changing the range in the second []
 
-qs= np.column_stack(n for n in features[2]) # one line only. 0 can be changed to use a different line
+qs= np.column_stack(n for n in features[0]) # one line only. 0 can be changed to use a different line
 
 ####
 # use the following two lines to do dimensionality reduction on the features matrix using PCA
