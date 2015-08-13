fc1ed1e3d04fbf287177a266462472841242e7f5
Aycha Tammour <aycha.tammour@gmail.com>
2015-08-13 18:26:51 -0400

    started a function to show reproducibility of clusters

---
+++ reproduce.py
@@ -0,0 +1,6 @@
+""" 13 Aug 2015
+a function to repeat the KMeans analysis many times and plot the mean and dispersion of the results of clustering (mean of coordiantes and dispersion as error bars).
+the goal is to show that the clustering is stable and the clusters we find are not random.
+"""
+
+
