7cba354e87cbc954c47c2a72bc5cfee7b4ead590
Labibahx <aycha.tammour@gmail.com>
2015-08-17 20:52:03 -0400

    merging

--- avgcompo.py
+++ avgcompo.py
@@ -1,3 +1,8 @@
 """13 Aug 2015 
 read names of fits files from fits files and create a mean composite spectrum from them
 """
+
+import numpy as np
+from astropy.io import fits
+from astropy.table import Table
+from scipy.stats import sigmaclip
