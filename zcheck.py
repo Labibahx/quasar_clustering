df5c78d4bc85901c977b3720cbf2b3e3e8278c10
Aycha Tammour <aycha.tammour@gmail.com>
2016-02-08 15:48:45 -0500

    quick func to check discrepancies in redshift estimates using different emission lines and the PCA estimate

---
+++ zcheck.py
@@ -0,0 +1,50 @@
+## quick script to check redshift inconsistancy among different clusters using the Anderson-Darling test
+
+from scipy import stats
+
+from astropy.table import Table, join
+
+
+def zcheck(line, sample, k):
+
+    ''' param: line used for clustering: c3, c4, mg2
+                sample: main, mixed, or bal
+                k: number of clusters: 3,4,5,6
+                
+    '''
+
+    if sample == "main":
+        s= ""
+    
+    elif sample == "mixed":
+        s= "mixed_"
+
+    else:
+        s= "bal_"
+
+    t= Table.read("sample_"+s+"myflags.fits") # open table with catalog data
+
+
+    c= Table.read("./clusters/"+line+"_"+str(k)+"clstrs_"+sample+".fits") # table with clustering results from a combination of K and line
+
+    tt= join(t, c, keys= "SDSS_NAME") # join tables to have both clustering results and data from catalog
+
+    # now calculate and print Anderson-Darling test for the redshift estimates using CIII], MgII, CIV and PCA. PCA is the one I used to shift spectra to restframe.
+
+    f= open("z_match.txt", 'wr')
+    f.write("Clstr"+'\t'+ "Num" + '\t' + "Z_MgII"+ '\t' + "sig"+ '\t'+ \
+            "Z_CIII"+ '\t' + "sig"+ '\t' +"Z_CIV"+ '\t' + "sig" + '\n')
+
+    s_mg, s_c3, s_c4 = [], [], []
+
+    for l in range(k):
+
+        s_mg= stats.anderson_ksamp([tt['Z_PCA'][tt['label']==l], tt['Z_MGII'][tt['label'] ==l]])
+        s_c3= stats.anderson_ksamp([tt['Z_PCA'][tt['label']==l], tt['Z_CIII'][tt['label'] ==l]])
+        s_c4= stats.anderson_ksamp([tt['Z_PCA'][tt['label']==l], tt['Z_CIV'][tt['label'] ==l]])
+
+        f.write('\t'+str(len(tt[tt['label'] == l]))+ \
+                '\t'+'{:5.3f}'.format(s_mg[0])+'\t'+'{:5.3f}'.format(s_mg[2])+ \
+                '\t'+'{:5.3f}'.format(s_c3[0])+'\t'+'{:5.3f}'.format(s_c3[2])+ \
+                '\t'+'{:5.3f}'.format(s_c4[0])+'\t'+'{:5.3f}'.format(s_c4[2])+'\n')
+
